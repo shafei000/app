3f1a0d2608b2247c47a3b50fcacb473697c8a959
shafei000 <shafeishafei00@gmail.com>
2024-10-28 13:06:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 # app.py
 import streamlit as st
-from transformers import AutoTokenizer, T5ForConditionalGeneration ,AutoModelForQuestionAnswering
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 import torch
 
-# Load the pre-trained T5 model
+# Set your Hugging Face access token
+HUGGINGFACE_TOKEN = "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"  # Replace with your actual token
+
+# Load the pre-trained model and tokenizer
 model_name = "chitchat00/chitchat0"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=HUGGINGFACE_TOKEN)
+model = AutoModelForQuestionAnswering.from_pretrained(model_name, use_auth_token=HUGGINGFACE_TOKEN)
 
 # Define a function to generate text
 def generate_text(input_text):
@@ -16,7 +19,7 @@ def generate_text(input_text):
     return tokenizer.decode(output_ids[0], skip_special_tokens=True)
 
 # Streamlit UI
-st.title("CHitchat Text Generation")
+st.title("Chitchat Text Generation")
 input_text = st.text_area("Enter your text:", "translate English to French: The house is wonderful.")
 if st.button("Generate"):
     output_text = generate_text(input_text)
